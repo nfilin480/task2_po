e4e252c232b4a52450611873cd408dbb2a3729b8
nfilin480 <61140093+nfilin480@users.noreply.github.com>
2020-04-29 19:12:44 +0300

    Update test_showtrimmedfromstart.h

--- tests/test_showtrimmedfromstart.h
+++ tests/test_showtrimmedfromstart.h
@@ -19,7 +19,6 @@ TEST(showtrimmedfromstart, standart){
     load (txt, filename);
 
     int newStdout = open(output_copy, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    
     int oldStdout = dup(STDOUT_FILENO);
     close(STDOUT_FILENO);
     dup2(newStdout, STDOUT_FILENO);
@@ -38,7 +37,6 @@ TEST(showtrimmedfromstart, standart){
     char *outBuf = (char *)malloc(sizeof(char)*1024);
     char *copyBuf = (char *)malloc(sizeof(char)*1024);
     int copyCount, outputCount;
-    free(output);
 
     copyCount = read(copy_file, copyBuf, 256);
     outputCount = read(output_file, outBuf, 256);
@@ -56,14 +54,13 @@ TEST(showtrimmedfromstart, standart){
 
 TEST(showtrimmedfromstart, no_standart_file){
     text txt = create_text();
-    char *filename = (char*) malloc(sizeof(char*) * 1024);
-    char *output_copy = (char*) malloc(sizeof(char*) * 1024);
-    snprintf(filename, 1024, "%s/input.txt",INPUTDIRFILESHOW);
-    snprintf(output_copy, 1024, "%s/output_copy.txt", INPUTDIRFILESHOW);
-    load (txt, filename);
+    char *file_name = (char*) malloc(sizeof(char*) * 1024);
+    char *copy = (char*) malloc(sizeof(char*) * 1024);
+    snprintf(file_name, 1024, "%s/input.txt",INPUTDIRFILESHOW);
+    snprintf(copy, 1024, "%s/output_copy.txt", INPUTDIRFILESHOW);
+    load (txt, file_name);
 
-    int newStdout = open(output_copy, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    
+    int newStdout = open(copy, O_WRONLY | O_CREAT | O_TRUNC, 0600);
     int oldStdout = dup(STDOUT_FILENO);
     close(STDOUT_FILENO);
     dup2(newStdout, STDOUT_FILENO);
@@ -74,28 +71,24 @@ TEST(showtrimmedfromstart, no_standart_file){
     close(newStdout);
     dup2(oldStdout, STDOUT_FILENO);
 
-    char *output = (char *)malloc(sizeof(char)*1024);
-    snprintf(output, 1024, "%s/output.txt", INPUTDIRFILESHOW);
+    char *output_orig = (char *)malloc(sizeof(char)*1024);
+    snprintf(output_orig, 1024, "%s/output.txt", INPUTDIRFILESHOW);
 
-    int copy_file = open(output_copy, O_RDONLY);
-    int output_file = open(output, O_RDONLY);
-    char *outBuf = (char *)malloc(sizeof(char)*1024);
-    char *copyBuf = (char *)malloc(sizeof(char)*1024);
-    int copyCount, outputCount;
-    free(output);
-
-    copyCount = read(copy_file, copyBuf, 256);
-    outputCount = read(output_file, outBuf, 256);
-    close(copy_file);
-    close(output_file);
-
-    ASSERT_EQ(copyCount, outputCount);
-    for(int i = 0; i < copyCount; i++)
-        ASSERT_EQ(outBuf[i], copyBuf[i]);
-
+    int copy_f = open(copy, O_RDONLY);
+    int output_f = open(output_orig, O_RDONLY);
+    char *bufOut = (char *)malloc(sizeof(char)*1024);
+    char *bufCopy = (char *)malloc(sizeof(char)*1024);
+    int counterCopy, counterOut;
+    
+    counterCopy = read(copy_f, bufCopy, 256);
+    counterOut = read(output_f, bufOut, 256);
+    close(copy_f);
+    close(output_f);
+    
+    ASSERT_EQ(counterCopy, counterOut);
+    for (int i = 0; i < counterCopy; i++)
+        ASSERT_EQ(bufOut[i], bufCopy[i]);
     remove_all(txt);
-
-
 }
 
 
